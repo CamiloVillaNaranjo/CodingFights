9e83303568d6a3e97e8c8df474cc6c75d6dbbc2a
Camilo A. Villa Naranjo <cavilla@nexos-software.com.co>
2019-05-22 13:33:57 -0500

    Se actuliza

--- CodingFigthsArcade/CodingFigthsArcade/ExploringWaters.cs
+++ CodingFigthsArcade/CodingFigthsArcade/ExploringWaters.cs
@@ -17,23 +17,45 @@ namespace CodingFigthsArcade
 
         static bool areSimilar(int[] a, int[] b)
         {
-            bool result = false;
-
+            int swaps = 0;
             int cE = 0;
-            int cD = 0;
-
-            for (int i = 0; i < a.Length; i++)
+            if (3 > a.Length || a.Length > (long)Math.Pow(10, 5))
+                return false;
+            else if (a.Length == b.Length)
             {
-                if (a[i] == b[i])
-                    cE++;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] != b[i])
+                    {
+                        swaps += 1;
+                    }
+                    else
+                    {
+                        cE += 1;
+                    }
+                }
+
+                if (swaps > 2) return false;
                 else
-                    cD++;
-                
+                {
+                    swaps = 0;
+                    a = a.OrderBy(x => x).ToArray();
+                    b = b.OrderBy(x => x).ToArray();
+
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        if (a[i] != b[i])
+                        {
+                            swaps += 1;
+                        }
+                    }
+                    return swaps == 0;
+                }
             }
-
-            if (cE > cD && cD <= 2) result = true;
-
-            return result;
+            else
+            {
+                return false;
+            }
         }
     }
 }
