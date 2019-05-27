55f0d7e9a411bd4d58da58e9c4d97f7d495899d0
Camilo A. Villa Naranjo <cavilla@nexos-software.com.co>
2019-05-27 16:57:53 -0500

    Se suben nuevos retos desarrollados

--- ConsoleApp/IslandOfKnowledge.cs
+++ ConsoleApp/IslandOfKnowledge.cs
@@ -110,34 +110,5 @@ namespace ConsoleApp
             return avoid;
         }
 
-        /*
-         Last night you partied a little too hard. Now there's a black and white photo of you that's 
-         about to go viral! You can't let this ruin your reputation, so you want to apply 
-         the box blur algorithm to the photo to hide its content.
-         
-         The pixels in the input image are represented as integers. The algorithm distorts the input 
-         image in the following way: Every pixel x in the output image has a value equal to the average 
-         value of the pixel values from the 3 × 3 square that has its center at x, including x itself. 
-         All the pixels on the border of x are then removed.
-
-         Return the blurred image as an integer, with the fractions rounded down.
-          */
-        static int[][] boxBlur(int[][] image)
-        {
-            int refVal = 3;
-            int b = image.Length / refVal;
-            b = (b * 3) - 1;
-            int[][] res;
-
-            for (int i = 0; i < refVal; i++)
-            {
-                for (int j = 0; j < refVal; j++)
-                {
-
-                }
-            }
-            
-            return image;
-        }
     }
 }
